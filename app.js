677ad9828d42b0c7d77d13a2c733d49f4092f794
Eric VanGeem <evangeem@citytechinc.com>
2015-02-23 08:44:11 -0600

    switch to ejs template rendering

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/reviews', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -54,6 +56,15 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: {}
     });
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('app listening at http://%s:%s', host, port);
+
 });
 
 
